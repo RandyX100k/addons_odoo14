bd3b8d599771b4258941a617da64a66476266250
Randy <ciprianrandy@gmail.com>
2025-12-28 18:43:10 -0400

    fix: agregando nuevo modulo para imprimir ticket 80mm

---
+++ addons/report_invoice/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "REPORT SHEILY",
+    'summary': """
+        Report Ticket - Order""",
+    'description': """
+        - Pedidos.
+        - Cotización.
+    """,
+    'author': "Randy Ciprian",
+    'website': "",
+    'category': "",
+    'version': '14.1',
+    'depends': ['base', 'sale'],
+    'data': [
+        'report/report_invoice_action.xml'
+    ],
+}
